e6ab7839964fe011ceffa777cf50aeadc8722857
hikaru-0602 <beretta0602@gmail.com>
2024-03-01 16:55:44 +0900

    テスト

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'firebase_options.dart';
 
+//テスト
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
