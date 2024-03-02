5172c70fd4bd7e8d59cda0d2517a676f542be8c6
hikaru-0602 <beretta0602@gmail.com>
2024-03-02 12:13:41 +0900

    シェアドプリファレンスに変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,176 +1,161 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'firebase_options.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-//テスト
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(const MyApp());
-}
-
-class DataService {
-  final db = FirebaseFirestore.instance;
-
-  Future<void> addMemo(String memo, int count) {
-    return db.collection('title').add({
-      'title': memo,
-      'count': count,
-    });
-  }
-
-  Stream<QuerySnapshot> getMemos() {
-    return db.collection('title').snapshots();
-  }
+void main() {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: '家の在庫管理アプリ'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title});
-
-  final String title;
-
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final DataService _dataService = DataService();
-  final TextEditingController _titleController = TextEditingController();
-  final TextEditingController _countController = TextEditingController();
+  final TextEditingController _itemController = TextEditingController();
+  final TextEditingController _quantityController = TextEditingController();
+  List<Map<String, dynamic>> itemList = [];
 
-  String _searchQuery = '';
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 254, 118, 108),
-        title: Text(widget.title),
+        title: const Text('在庫管理システム'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                resetData();
+              },
+              icon: const Icon(Icons.abc))
+        ],
       ),
       body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: _titleController,
-              onChanged: (value) {
-                setState(() {
-                  _searchQuery = value;
-                });
-              },
-              decoration: InputDecoration(
-                labelText: '検索',
-                prefixIcon: Icon(Icons.search),
-              ),
-            ),
+          const Text(
+            "在庫一覧",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
           ),
           Expanded(
-            child: StreamBuilder(
-              stream: _dataService.getMemos(),
-              builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                if (snapshot.hasData) {
-                  var memos = snapshot.data!.docs.where((memo) {
-                    var title = memo.data() as Map<String, dynamic>;
-                    return title['title'].toString().contains(_searchQuery);
-                  }).toList();
-
-                  return ListView.builder(
-                    itemCount: memos.length,
-                    itemBuilder: (context, index) {
-                      var title = memos[index].data() as Map<String, dynamic>;
-                      return Card(
-                        child: ListTile(
-                          leading: Container(
-                            width: 40,
-                          ),
-                          title: Text(
-                            '${title['title']}',
-                            style: const TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          subtitle: Text('個数: ${title['count']}'),
-                        ),
-                      );
-                    },
-                  );
-                } else if (snapshot.hasError) {
-                  return Text('Error: ${snapshot.error}');
-                }
-                return CircularProgressIndicator();
+            child: ListView.builder(
+              itemCount: itemList.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text('商品名: ${itemList[index]["item"]}'),
+                  subtitle: Text('個数: ${itemList[index]["quantity"]}'),
+                );
               },
             ),
           ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => _showAddDialog(context),
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: () {
+          _showAddDialog();
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
 
-  Future<void> _showAddDialog(BuildContext context) async {
-    return showDialog<void>(
+  void _showAddDialog() {
+    showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text('Add Memo'),
+          title: Text('商品を追加'),
           content: Column(
             children: [
               TextField(
-                controller: _titleController,
-                decoration: InputDecoration(labelText: '名前'),
+                controller: _itemController,
+                decoration: InputDecoration(labelText: '商品名'),
               ),
               TextField(
-                controller: _countController,
-                decoration: InputDecoration(labelText: '個数'),
+                controller: _quantityController,
                 keyboardType: TextInputType.number,
+                decoration: const InputDecoration(labelText: '個数'),
               ),
             ],
           ),
-          actions: <Widget>[
+          actions: [
             TextButton(
               onPressed: () {
-                Navigator.of(context).pop();
+                Navigator.pop(context);
+                _itemController.clear();
+                _quantityController.clear();
               },
               child: Text('キャンセル'),
             ),
             TextButton(
-              onPressed: () async {
-                String title = _titleController.text;
-                int count = int.tryParse(_countController.text) ?? 0;
-
-                if (title.isNotEmpty && count > 0) {
-                  await _dataService.addMemo(title, count);
-                  _titleController.clear();
-                  _countController.clear();
-                  Navigator.of(context).pop();
-                }
+              onPressed: () {
+                saveData();
+                Navigator.pop(context);
               },
-              child: Text('Add'),
+              child: Text('保存'),
             ),
           ],
         );
       },
     );
+    _itemController.clear();
+    _quantityController.clear();
+  }
+
+  Future<void> saveData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String item = _itemController.text;
+    int quantity = int.parse(_quantityController.text);
+
+    // キーと値を保存
+    prefs.setString(item, quantity.toString());
+
+    // 保存後に入力欄をクリア
+    _itemController.clear();
+    _quantityController.clear();
+
+    // リストを更新
+    loadData();
+  }
+
+  Future<void> loadData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    // 全てのキーを取得
+    List<String> keys = prefs.getKeys().toList();
+
+    List<Map<String, dynamic>> updatedList = [];
+
+    for (String key in keys) {
+      String item = key;
+      int quantity = int.tryParse(prefs.getString(key) ?? '0') ?? 0;
+      updatedList.add({"item": item, "quantity": quantity});
+    }
+
+    setState(() {
+      // リストを更新
+      itemList = updatedList;
+    });
+  }
+
+  Future<void> resetData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.clear();
+    setState(() {
+      saveData();
+      loadData();
+    });
   }
 }
